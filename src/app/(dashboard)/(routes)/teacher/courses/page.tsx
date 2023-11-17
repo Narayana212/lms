879e179f6cbf8a1f21f2aeda8dc35ebf62618479
Raavi Narayana <114774469+Narayana212@users.noreply.github.com>
2023-11-17 23:42:18 +0530

    course upload fincnality

--- src/app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ src/app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,7 +1,13 @@
+import { buttonVariants } from '@/components/ui/button'
+import Link from 'next/link'
 import React from 'react'
 
 export default function page() {
   return (
-    <div>page</div>
+    <div>
+      <Link href="/teachers/create" className={buttonVariants()}>
+        Create Course
+      </Link>
+    </div>
   )
 }
--- src/app/(dashboard)/(routes)/teacher/create/page.tsx
+++ src/app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import * as z from "zod";
 import axios from "axios";
 import { zodResolver } from "@hookform/resolvers/zod";
